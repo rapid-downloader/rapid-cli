7f04a9affb9b34328fda0b585efe891c54091117
thoriqadillah <thoriqadillah59@gmail.com>
2023-08-20 12:04:47 +0700

    working POC

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/goccy/go-json"
+	"github.com/gorilla/websocket"
 )
 
 type progressBar struct {
@@ -24,26 +27,59 @@ func (p *progressBar) String() string {
 	return buff.String()
 }
 
+type entry struct {
+	Id               string `json:"id"`
+	Name             string `json:"name"`
+	Location         string `json:"location"`
+	Size             int64  `json:"size"`
+	Filetype         string `json:"filetype"`
+	URL              string `json:"url"`
+	Resumable        bool   `json:"resumable"`
+	ChunkLen         int    `json:"chunkLen"`
+	DownloadProvider string `json:"downloadProvider"`
+}
+
+func (e *entry) String() string {
+	var buffer bytes.Buffer
+
+	buffer.WriteString(fmt.Sprintf("ID: %v\n", e.Id))
+	buffer.WriteString(fmt.Sprintf("Name: %v\n", e.Name))
+	buffer.WriteString(fmt.Sprintf("Location: %v\n", e.Location))
+	buffer.WriteString(fmt.Sprintf("Size: %v\n", e.Size))
+	buffer.WriteString(fmt.Sprintf("Filetype: %v\n", e.Filetype))
+	buffer.WriteString(fmt.Sprintf("Resumable: %v\n", e.Resumable))
+	buffer.WriteString(fmt.Sprintf("ChunkLen: %v\n", e.ChunkLen))
+
+	return buffer.String()
+}
+
+type responseBody struct {
+	Status string      `json:"status"`
+	Data   interface{} `json:"data"`
+}
+
 type body struct {
 	Url string `json:"url"`
 }
+
+type Map map[string]interface{}
 
 const serverWs = "ws://localhost:3300/ws/cli"
 const server = "http://localhost:3300/download/cli"
 
 func main() {
-	// interrupt := make(chan os.Signal, 1)
-	// signal.Notify(interrupt, os.Interrupt)
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
 
-	// conn, response, err := websocket.DefaultDialer.Dial(serverWs, nil)
-	// if err != nil {
-	// 	log.Fatalf("Error connecting websocket: %v. Status code %d", err, response.StatusCode)
-	// }
+	conn, res, err := websocket.DefaultDialer.Dial(serverWs, nil)
+	if err != nil {
+		log.Fatalf("Error connecting websocket: %v. Status code %d", err, res.StatusCode)
+	}
 
-	// defer conn.Close()
+	defer conn.Close()
 
 	body := body{
-		Url: "https://rr5---sn-vgqsknz7.googlevideo.com/videoplayback?expire=1692478697&ei=idjgZJFRmeWKvg_zwZ6ICw&ip=212.102.59.151&id=o-AKd6YtvaPJyKVzEWnEpnDJlSTUtRk__kuNbuQM-HEQuR&itag=22&source=youtube&requiressl=yes&mh=1P&mm=31%2C26&mn=sn-vgqsknz7%2Csn-p5qlsn6l&ms=au%2Conr&mv=m&mvi=5&pl=26&initcwndbps=642500&spc=UWF9f9Ji9MprP7GV-lx_MwWnbEO7xYE&vprv=1&svpuc=1&mime=video%2Fmp4&cnr=14&ratebypass=yes&dur=8673.790&lmt=1682606470933426&mt=1692456802&fvip=5&fexp=24007246%2C24363392&c=ANDROID&txp=5318224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRgIhAI7PbAZk-M6T2h7v-mGhjKg3oeAMhoERXYUF4zmMRIjsAiEAyb0p5WHTsxVfenCbrbH8p5mU4PgR_GIFcdCeD0iFbwI%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRAIgR6rk9MR9p3bEmTClFzDO_oSzZoybxaExcVF6K8SPXDsCIGykg36QfBJGwo5lDrabitVv_DTR3pGcc2IJxu4_jbWw&title=Mastering%20WebSockets%20With%20Go%20-%20An%20in-depth%20tutorial",
+		Url: "https://link.testfile.org/PDF50MB",
 	}
 
 	payload, err := json.Marshal(body)
@@ -51,71 +87,71 @@ func main() {
 		log.Fatal("Error marshalling body:", err)
 	}
 
-	res, err := http.DefaultClient.Post(server, "application/json", bytes.NewBuffer(payload))
+	response, err := http.DefaultClient.Post(server, "application/json", bytes.NewBuffer(payload))
 	if err != nil {
 		log.Fatal("Error performing http post:", err)
 	}
 
-	defer res.Body.Close()
+	defer response.Body.Close()
 
 	var buffer bytes.Buffer
-	if _, err := buffer.ReadFrom(res.Body); err != nil {
+	if _, err := buffer.ReadFrom(response.Body); err != nil {
 		log.Fatal(err)
 	}
 
-	var result interface{}
+	var result responseBody
 	if err := json.Unmarshal(buffer.Bytes(), &result); err != nil {
 		log.Fatal("Error unmarshalling buffer:", err)
 	}
 
-	log.Println(result)
+	if entry, ok := result.Data.(entry); ok {
+		log.Println(entry.String())
+	}
 
-	// done := make(chan struct{})
+	done := make(chan struct{})
 
-	// go func() {
-	// 	for {
-	// 		select {
-	// 		case <-interrupt:
-	// 			return
-	// 		default:
-	// 			_, message, err := conn.ReadMessage()
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case <-interrupt:
+				return
+			default:
+				_, message, err := conn.ReadMessage()
 
-	// 			if err != nil {
-	// 				log.Println("Error reading message:", err)
-	// 				return
-	// 			}
+				if err != nil {
+					log.Println("Error reading message:", err)
+					return
+				}
 
-	// 			var progress progressBar
-	// 			if err := json.Unmarshal(message, &progress); err != nil {
-	// 				log.Println("Error unmarshalling message:", err)
-	// 				return
-	// 			}
+				var msg Map
+				if err := json.Unmarshal(message, &msg); err != nil {
+					log.Println("Error unmarshalling message:", err)
+					return
+				}
 
-	// 			log.Println(progress)
+				if _, ok := msg["done"]; ok {
+					done <- struct{}{}
+					return
+				}
 
-	// 			if progress.Done {
-	// 				done <- struct{}{}
-	// 				return
-	// 			}
-	// 		}
-	// 	}
-	// }()
+			}
+		}
+	}()
 
-	// ticker := time.NewTicker(time.Second)
-	// defer ticker.Stop()
+	for {
+		select {
+		case <-done:
+			return
+		case <-interrupt:
+			log.Println("Shutting down...")
+			if err := conn.WriteMessage(websocket.TextMessage, []byte("G	ood bye...")); err != nil {
+				log.Println("Error sending message:", err)
+				return
+			}
 
-	// for {
-	// 	select {
-	// 	case <-done:
-	// 		return
-	// 	case <-interrupt:
-	// 		log.Println("Shutting down...")
-	// 		if err := conn.WriteMessage(websocket.TextMessage, []byte("G	ood bye...")); err != nil {
-	// 			log.Println("Error sending message:", err)
-	// 			return
-	// 		}
-
-	// 		return
-	// 	}
-	// }
+			return
+		}
+	}
 }
